9c500329661b7fdafd42c2dd04a2e7a3c9302af8
aiitfuture1 <34682682+aiitfuture1@users.noreply.github.com>
2019-01-10 15:33:32 +0300

    Update visualization.py

--- keras_retinanet/utils/visualization.py
+++ keras_retinanet/utils/visualization.py
@@ -43,7 +43,7 @@ def draw_caption(image, box, caption):
     """
     b = np.array(box).astype(int)
     
-    cv2.putText(image, caption, (b[0], b[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+    cv2.putText(image, caption, (b[0], b[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
     cv2.putText(image, caption, (b[0], b[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 1)
 
 
